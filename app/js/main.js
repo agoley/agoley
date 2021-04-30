79487dbe46f313c209676e00f1760c83da1bc639
Alex Goley <ajgoley@gmail.com>
2021-04-29 22:24:37 -0400

    mobile input WIP

--- app/js/main.js
+++ app/js/main.js
@@ -345,8 +345,8 @@ var CommandFactory = /*#__PURE__*/function () {
 
     if (isMobile.any()) {
       console.log("is mobile");
-      this.input = document.createElement("input"); // this.input.style.display = "none";
-
+      this.input = document.createElement("input");
+      this.input.style.opacity = "0";
       document.body.appendChild(this.input);
 
       this.input.onclick = function (e) {
